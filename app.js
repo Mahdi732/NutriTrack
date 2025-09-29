74bd6617a5453f7220e005dd9aeb5c10e15d65b6
bourraOmar <bwrtmr@gmail.com>
2025-09-29 15:29:15 +0100

    fixing the routing

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 import express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+import auth from "./routes/route.js";
 
 dotenv.config()
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -10,6 +19,8 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.send("Server is running!");
 });
+
+app.use('/auth', auth);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on http://localhost:${process.env.PORT}`);
